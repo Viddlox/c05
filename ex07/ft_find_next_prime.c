c5391547f8ab75adeab9bfeff3b700e3a7ced82c
ijon <itan@student.42kl.edu.my>
2022-09-01 13:00:46 +0800

    first commit

---
+++ ex07/ft_find_next_prime.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/25 20:39:43 by itan              #+#    #+#             */
+/*   Updated: 2022/09/01 12:45:00 by itan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb == 1)
+		return (0);
+	i = 2;
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	int	i;
+
+	i = nb;
+	while (true)
+	{
+		if (ft_is_prime(i))
+			return (i);
+		else
+			i++;
+	}
+}
+int	main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%d", ft_find_next_prime(atoi(argv[1])));
+	return (0);
+}
