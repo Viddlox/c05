94232721c9756733b945416d2716a8cd05cb803e
ijon <itan@student.42kl.edu.my>
2022-09-03 13:10:31 +0800

    last try (hopefully)

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 17:26:58 by itan              #+#    #+#             */
-/*   Updated: 2022/09/01 17:10:45 by itan             ###   ########.fr       */
+/*   Updated: 2022/09/03 13:09:33 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int	ft_iterative_power(int nb, int power)
 	if (power == 0)
 		return (1);
 	value = nb;
-	power++;
 	while (--power > 0)
 		value *= nb;
 	return (value);
